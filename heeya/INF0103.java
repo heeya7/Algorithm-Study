d4f23d075fa498319d11fbcf83f17d5ea58485fc
heeya7 <whtp706@naver.com>
2023-08-30 00:25:56 +0900

    [INF] 1-3/문장속단어/초급

---
+++ heeya/INF0103.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String solution(String str) {
+        String answer = "";
+        int maxLen = 0;
+        String[] strArr = str.split(" ");
+
+        for(String x : strArr) {
+            int len = x.length();
+            if(len > maxLen) {
+                maxLen = len;
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+
+        System.out.println(solution(str));
+    }
+}
