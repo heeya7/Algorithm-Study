34102453c74b28f617de87ce11840373db7dd2b9
heeya7 <whtp706@naver.com>
2023-09-05 22:19:32 +0900

    [INF] 1-6/중복문자제거/초급

---
+++ heeya/INF0106.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String solution(String str) {
+        String answer ="";
+
+        for(int i=0; i<str.length(); i++) {
+            if(i == str.indexOf(str.charAt(i))) {
+                answer +=str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+
+        System.out.println(solution(str));
+
+    }
+}
