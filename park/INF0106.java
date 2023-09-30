f6d4031dc35057b72ff5445fcbe923027612f68c
kyunghyun-park <qqqq_o@naver.com>
2023-09-30 16:42:11 +0900

    [INF] 1-6/중복문자제거/초급

---
+++ park/INF0106.java
@@ -0,0 +1,28 @@
+package park;
+
+import java.util.Scanner;
+
+public class INF0106 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        INF0106 inf = new INF0106();
+
+        System.out.println(inf.solution(s));
+    }
+
+    public String solution(String s) {
+        String answer = "";
+        String duplication = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(duplication.indexOf(c) < 0) {
+                answer += c;
+                duplication += c;
+            }
+        }
+
+        return answer;
+    }
+}
