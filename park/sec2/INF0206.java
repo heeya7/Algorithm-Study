b3f98ac7f21eca8dd81d181ea5f4542e24f593d9
kyunghyun-park <qqqq_o@naver.com>
2023-10-13 23:29:21 +0900

    [INF] 2-6/뒤집은 소수/초급

---
+++ park/sec2/INF0206.java
@@ -0,0 +1,43 @@
+package park.sec2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class INF0206 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        INF0206 inf = new INF0206();
+        for (int x : inf.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int tmp=arr[i];
+            int res=0;
+            while(tmp>0){
+                int t=tmp%10;
+                res=res*10+t;
+                tmp=tmp/10;
+            }
+            if(isPrime(res)) answer.add(res);
+        }
+        return answer;
+    }
+
+    public boolean isPrime(int num){
+        if(num==1) return false;
+        for(int i=2; i<num; i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+}
