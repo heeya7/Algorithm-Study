d13da7d7738d453b06e961c636aeede7f65e0d94
hyunyyxp <qqqq_o@naver.com>
2023-10-16 17:31:54 +0900

    [INF] 2-6/뒤집은 소수/초급

--- park/sec2/INF0206.java
+++ park/sec2/INF0206.java
@@ -24,24 +24,28 @@ public class INF0206 {
     public ArrayList<Integer> solution(int[] arr) {
         ArrayList<Integer> answerList = new ArrayList<>();
 
-        for (int i : arr) {
-            int res = Integer.parseInt(new StringBuilder(String.valueOf(i)).reverse().toString());
+//        for (int i : arr) {
+//            int res = Integer.parseInt(new StringBuilder(String.valueOf(i)).reverse().toString());
+//            if(isPrime(res)) {
+//                answerList.add(res);
+//            }
+//        }
+
+        /* 직접 뒤집기 */
+        for(int i=0; i<arr.length; i++){
+            int tmp=arr[i];
+            int res=0;
+
+            while(tmp>0){
+                int t=tmp%10;
+                res=res*10+t;
+                tmp=tmp/10;
+            }
+
             if(isPrime(res)) {
                 answerList.add(res);
             }
         }
-
-        /* 직접 뒤집기 */
-//        for(int i=0; i<n; i++){
-//            int tmp=arr[i];
-//            int res=0;
-//            while(tmp>0){
-//                int t=tmp%10;
-//                res=res*10+t;
-//                tmp=tmp/10;
-//            }
-//            if(isPrime(res)) answer.add(res);
-//        }
 
         return answerList;
     }
