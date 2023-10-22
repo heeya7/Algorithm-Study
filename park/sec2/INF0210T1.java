555bc416fa2fe933dc2df8fe787a6d8f68fe5103
kyunghyun-park <qqqq_o@naver.com>
2023-10-22 15:14:34 +0900

    [INF] 2-10/봉우리/초급

--- park/sec2/INF0210T1.java
+++ park/sec2/INF0210T1.java
@@ -3,36 +3,38 @@ package park.sec2;
 import java.util.Scanner;
 
 public class INF0210T1 {
-    int[] dx={-1, 0, 1, 0};
-    int[] dy={0, 1, 0, -1};
-
     public static void main(String[] args){
         INF0210T1 T = new INF0210T1();
         Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int[][] arr=new int[n][n];
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                arr[i][j]=kb.nextInt();
+                arr[i][j] = kb.nextInt();
             }
         }
         System.out.print(T.solution(n, arr));
     }
 
     public int solution(int n, int[][] arr){
-        int answer=0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        int answer = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                boolean flag=true;
+                boolean flag = true;
                 for(int k=0; k<4; k++){
-                    int nx=i+dx[k];
-                    int ny=j+dy[k];
-                    if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]>=arr[i][j]){
-                        flag=false;
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]){
+                        flag = false;
                         break;
                     }
                 }
-                if(flag) answer++;
+                if(flag) {
+                    answer++;
+                }
             }
         }
         return answer;
