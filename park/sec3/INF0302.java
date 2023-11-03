ea7b0b9be18890f39c314170aa85ed2b8c3e1c3b
hyunyyxp <qqqq_o@naver.com>
2023-11-03 15:03:14 +0900

    [INF] 3-2/공통원소 구하기/초급

---
+++ park/sec3/INF0302.java
@@ -0,0 +1,47 @@
+package park.sec3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class INF0302 {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] == b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+
+        INF0302 inf = new INF0302();
+        for (int x : inf.solution(n, m, a, b)) {
+            System.out.print(x + " ");
+        }
+    }
+}
